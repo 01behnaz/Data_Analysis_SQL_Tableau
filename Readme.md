62f70a747d1caa0692fe14a4f24ac3acc80a3122
01behnaz <105924347+01behnaz@users.noreply.github.com>
2023-12-30 22:13:09 +0330

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# COVID-19 Data Analysis: Infection and Death using Tableau & SQL
+
+In this project, we used SQL for Data Exploration and then visualized the data in Tableau.
+
+
+## Prerequisites
+
++ SQL Server
++ Tableau
+
+
+## Data Source
+
+[COVID-19 Dataset](https://ourworldindata.org/covid-deaths)
+
+
+## Tableau Dashboard
+
+Examining the total number of deaths caused by COVID in the world and different continents, as well as the average infected population in densely populated and developed countries and the Middle East from the beginning to now and the forecast for the next year.
+
+![Dashboard Screenshot](Screenshot.png)
---
+++ SQLQuery.sql
@@ -0,0 +1,134 @@
+﻿
+--Select data that we are going to be using
+
+select location, date, total_cases, new_cases, total_deaths, population
+from PortfolioProject..CovidDeaths
+where continent is not null
+order by 1,2
+
+
+-- looking at Total cases vs Total deaths
+-- shows the likelihood of dying if you contract covid in your country
+
+SELECT location, date, total_cases, total_deaths,
+CAST((CAST(total_deaths AS FLOAT) / CAST(total_cases AS FLOAT)) * 100 AS FLOAT) AS Death_percentage
+FROM PortfolioProject..CovidDeaths
+where location like '%iran%'
+and continent is not null
+order by 1,2
+
+
+-- looking at Total Cases vs Population
+-- shows what percentage of the population got covid
+
+SELECT location, date, population, total_cases,
+CAST((CAST(total_cases AS FLOAT) / CAST(population AS FLOAT)) * 100 AS FLOAT) AS PercentPopulationInfected
+FROM PortfolioProject..CovidDeaths
+where continent is not null
+order by 1,2
+
+
+-- looking at countries with the Highest infection Rate compared to the Population
+
+SELECT location, population, MAX(total_cases) as HighestInfectionCount,
+MAX(CAST(CAST(total_cases AS FLOAT) / NULLIF(CAST(population AS FLOAT), 0) * 100 AS FLOAT)) AS PercentPopulationInfected
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null 
+GROUP BY location, population
+ORDER BY PercentPopulationInfected desc
+
+
+-- showing Countries with Highest Death Count per Population
+
+SELECT location,
+MAX(cast(total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+where continent is not null
+Group by location
+order by TotalDeathCount desc
+
+
+-- break things down by continent
+
+SELECT
+continent,
+MAX(cast(total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+where continent is not null
+Group by continent
+order by TotalDeathCount desc
+
+
+-- showing continents with the highest death count per population
+
+SELECT continent, MAX(cast(total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+where continent is not null
+Group by continent
+order by TotalDeathCount desc
+
+
+-- join 2 table together
+
+select * 
+from PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
+on dea.location = vac.location
+and dea.date = vac.date
+
+
+-- looking at total population vs vaccinations
+-- all people vaccinated in the world
+
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+from PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
+on dea.location = vac.location
+and dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+order by 2,3
+
+
+/*
+Queries used for Tableau
+*/
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- 'European Union' is part of Europe and 'world' is the sum of all continents
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International', 'High income', 'Low income', 'Upper middle income', 'Lower middle income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+
+
